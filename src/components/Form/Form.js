c65dd3309f7614bd9d359f92b41a879729ab2870
srabon0 <srabonemam4@gmail.com>
2022-05-18 22:22:56 +0600

    firebase set up

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,8 +1,16 @@
 import React from "react";
 
 const Form = () => {
+    const handleAddTask = (event)=>{
+        event.preventDefault()
+        const title = event.target.title.value
+        const description = event.target.description.value
+        console.log(title,description);
+
+    }
+
   return (
-    <form>
+    <form onSubmit={handleAddTask}>
       <div class="mb-3">
         <label class="form-label">Task Title</label>
         <input type="text" class="form-control" name="title" />
